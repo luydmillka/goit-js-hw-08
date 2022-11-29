e00300e767cebbb144ed09c3b8fded849bf3873b
luydmillka <luydmilka@3g.ua>
2022-11-29 14:39:30 +0200

    виправлено дані в package.json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const galleryItemsContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryMarkup(galleryItems);
+
+galleryItemsContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+galleryItemsContainer.addEventListener("click", onGalleryItemsContainerClick);
+
+function onGalleryItemsContainerClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+}
+
+function galleryMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join('');
+}
