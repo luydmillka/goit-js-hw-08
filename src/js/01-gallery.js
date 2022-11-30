46902b34a7f108c0db2de2aed51b55ce203ac747
luydmillka <luydmilka@3g.ua>
2022-11-30 09:54:20 +0200

    task 1-2 added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,8 +5,6 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
-
 const galleryItemsContainer = document.querySelector('.gallery');
 const galleryItemsMarkup = galleryMarkup(galleryItems);
 
@@ -14,11 +12,11 @@ galleryItemsContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
 galleryItemsContainer.addEventListener("click", onGalleryItemsContainerClick);
 
 function onGalleryItemsContainerClick(evt) {
+    console.log(evt.target)
     evt.preventDefault();
     if (!evt.target.classList.contains('gallery__image')) {
         return;
     }
-
 }
 
 function galleryMarkup(items) {
@@ -28,3 +26,10 @@ function galleryMarkup(items) {
         </a>`
     }).join('');
 }
+
+let galleryLightBox = new SimpleLightbox('.gallery__item', {
+        capture: true,
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+onVideoStart()
+
+player.on('timeupdate', throttle(onVideoPlayed, 1000));
+
+function onVideoPlayed(data) {
+    const playerCurrentTime = data.seconds
+    console.log(playerCurrentTime)
+    localStorage.setItem('videoplayer-current-time', playerCurrentTime)
+}
+
+
+function onVideoStart() {
+    const savedTime = localStorage.getItem('videoplayer-current-time')
+
+    if (savedTime) {
+        player.setCurrentTime(savedTime).then(function(seconds) {
+
+        }).catch(function(error) {
+       switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+    });
+    }
+}
