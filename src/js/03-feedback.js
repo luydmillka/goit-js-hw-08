1078e3207d052d681ec44b10da0e8a5cb7b33426
luydmillka <luydmilka@3g.ua>
2022-11-30 15:43:48 +0200

    updated 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea')
+}
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFormFieldInput);
+
+onFormReloaded();
+
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    evt.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+function onFormFieldInput(evt) {
+    const inputValue = evt.target.value;
+    formData[evt.target.name] = inputValue;
+
+    const formDataStringifyed = JSON.stringify(formData);
+    localStorage.setItem("feedback-form-state", formDataStringifyed);
+ }
+
+function onFormReloaded() {
+    const savedFormFields = localStorage.getItem("feedback-form-state");
+    const parcedFormFields = JSON.parse(savedFormFields);
+
+    if (parcedFormFields) {
+        refs.input.value = parcedFormFields.email;
+        refs.textarea.value = parcedFormFields.message;
+    }
+ }
+
+
